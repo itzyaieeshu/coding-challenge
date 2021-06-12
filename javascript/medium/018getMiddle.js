ceb820a9754b7e6ad8c3eb5eef9f4fa8c4c87aca
itzyaieeshu <62168657+itzyaieeshu@users.noreply.github.com>
2021-06-12 22:01:49 +1000

    get middle char update

---
+++ javascript/medium/018getMiddle.js
@@ -0,0 +1,17 @@
+const getMiddle = (str) => ((str.length % 2 === 0) ? str.charAt(Math.floor(str.length / 2) - 1) + str.charAt(Math.floor((str.length / 2)))
+  : (str.length === 1) ? str
+    : str.charAt(Math.floor(str.length / 2)));
+
+// Return the Middle Character(s) of a String
+// Create a function that takes a string and returns the middle character(s). If the word's length is odd, return the middle character. If the word's length is even, return the middle two characters.
+
+// Examples
+// getMiddle("test") ➞ "es"
+
+// getMiddle("testing") ➞ "t"
+
+// getMiddle("middle") ➞ "dd"
+
+// getMiddle("A") ➞ "A"
+// Notes
+// All test cases contain a single word (as a string).
