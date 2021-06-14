c2a6194fb49e3d2acb452e355e4c496d7800bc49
itzyaieeshu <62168657+itzyaieeshu@users.noreply.github.com>
2021-06-14 23:12:43 +1000

    get middle character added

--- javascript/medium/018getMiddle.js
+++ javascript/medium/018getMiddle.js
@@ -1,6 +1,11 @@
-const getMiddle = (str) => ((str.length % 2 === 0) ? str.charAt(Math.floor(str.length / 2) - 1) + str.charAt(Math.floor((str.length / 2)))
-  : (str.length === 1) ? str
-    : str.charAt(Math.floor(str.length / 2)));
+const getMiddle = (str) => {
+  if (str.length % 2 === 0) {
+    return str.charAt(Math.floor(str.length / 2) - 1) + str.charAt(Math.floor((str.length / 2)));
+  } if (str.length === 1) {
+    return str;
+  }
+  return str.charAt(Math.floor(str.length / 2));
+};
 
 // Return the Middle Character(s) of a String
 // Create a function that takes a string and returns the middle character(s). If the word's length is odd, return the middle character. If the word's length is even, return the middle two characters.
