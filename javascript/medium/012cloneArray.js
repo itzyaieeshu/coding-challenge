4a8fc613a0eb589b786155e0b992dd767bbccff9
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-20 23:55:36 +1000

    even last test minor update

--- javascript/medium/012cloneArray.js
+++ javascript/medium/012cloneArray.js
@@ -5,6 +5,8 @@ const clone = (arr) => {
   return result;
 };
 
+module.exports = clone;
+
 // Clone an Array
 // The Code tab has a code which attempts to add a clone of an array to itself. There is no error message, but the results are not as expected. Can you fix the code?
 
