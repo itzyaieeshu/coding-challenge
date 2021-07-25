804010f5faa665c367272798dcd9caaf0f9f9515
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-26 00:35:48 +1000

    Diagonal of cone test case update

--- javascript/tests/medium/015diagonalOfCube.test.js
+++ javascript/tests/medium/015diagonalOfCube.test.js
@@ -1,7 +1,7 @@
 const cubeDiagonal = require('../../medium/015diagonalOfCube');
 
-test('cubeDiagonal(8) returns 3.42', () => {
-    expect(cubeDiagonal(8)).toEqual(3.42);
+test('cubeDiagonal(8) returns 3.46', () => {
+    expect(cubeDiagonal(8)).toEqual(3.46);
 });
 
 test('cubeDiagonal(1157.625) returns 18.19', () => {
